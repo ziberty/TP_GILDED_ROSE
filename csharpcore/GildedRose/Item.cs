bee47e8118b21cdeda39df18f5a2d01cd591f7d7
Hugo <43464879+ziberty@users.noreply.github.com>
2022-10-24 11:23:08 +0200

    updateQuality method

---
+++ csharpcore/GildedRose/Item.cs
@@ -0,0 +1,71 @@
+﻿namespace GildedRoseKata
+{
+    public class Item
+    {
+        private string name;
+        private int sellIn;
+        private int quality;
+
+        public Item(string name, int sellIn, int quality)
+        {
+            this.name = name;
+            this.sellIn = sellIn;
+            this.quality = sellIn;
+        }
+
+        public void updateQuality()
+        {
+            switch (name)
+            {
+                case "Aged Brie":
+                    quality++;
+                    break;
+                case "Backstage passes":
+                    switch (sellIn)
+                    {
+                        case <= 0:
+                            quality = 0;
+                            break;
+                        case <= 5:
+                            quality += 3;
+                            break;
+                        case <= 10:
+                            quality += 2;
+                            break;
+                        default:
+                            quality++;
+                            break;
+                    }
+                    break;
+                default:
+                    if (sellIn > 0)
+                    {
+                        sellIn--;
+                        quality--;
+                    }
+                    else
+                    {
+                        quality -= 2;
+                    }
+                    break;
+            }
+            itemQualityCheck();
+        }
+        
+        private void itemQualityCheck()
+        {
+            if (quality < 0)
+            {
+                quality = 0;
+            }
+            if (quality > 50)
+            {
+                quality = 50;
+            }
+            if (name == "Sulfuras")
+            {
+                quality = 80;
+            }
+        }
+    }
+}
